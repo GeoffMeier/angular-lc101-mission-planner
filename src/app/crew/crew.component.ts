832197689fd06aa3fa9d7530ce41484002682cfd
Geoffrey Meier <grm0809@gmail.com>
2022-12-13 16:45:41 -0600

    angular-studio MissionPlanner

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -1,23 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-crew',
-  templateUrl: './crew.component.html',
-  styleUrls: ['./crew.component.css']
+   selector: 'app-crew',
+   templateUrl: './crew.component.html',
+   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
   memberBeingEdited: object = null;
-  crew: object[] = [
-    {name: "Eileen Collins", firstMission: false},
-    {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
-  ];
-  
-  constructor() { }
-
-  ngOnInit() {
-  }
-  add(memberName: string, isFirst: boolean) {
+   crew: object[] = [
+      {name: "Eileen Collins", firstMission: false},
+      {name: "Mae Jemison", firstMission: false},
+      {name: "Ellen Ochoa", firstMission: true}
+    
+   ];
+   add(memberName: string, isFirst: boolean) {
     this.crew.push({name: memberName, firstMission: isFirst});
   }
   remove(member: object) {
@@ -25,8 +21,13 @@ export class CrewComponent implements OnInit {
     this.crew.splice(index, 1);
   }
   edit(member: object) {
+    
     this.memberBeingEdited = member;
  }
+  
+   constructor() { }
+
+   ngOnInit() {
+   }
 
 }
-
